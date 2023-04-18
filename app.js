9ec97d8269fc6b9d64611729f0c6691e1ffcf1e0
Vinicius Costa e Paula <vinicius.paula@services.cmsoftware.com.br>
2023-04-18 15:39:28 -0300

    Voltando para o git anterior

--- app.js
+++ app.js
@@ -1,5 +1,1 @@
 console.log("rodando o sistema de gerenciamento de jogos")
-console.log("aplicaçao está se conectando ao banco de dados")
-
-console.log ("O paulo fez esse commit se voce ver na aula!")
-console.log ("enviando para o github")
