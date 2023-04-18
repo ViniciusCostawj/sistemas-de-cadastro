915cf37b187525c135edefb19cd011f0fef038b0
Vinicius Costa e Paula <vinicius.paula@services.cmsoftware.com.br>
2023-04-18 15:03:06 -0300

    adicionando um console log

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
 console.log("rodando o sistema de gerenciamento de jogos")
+console.log("aplicaçao está se conectando ao banco de dados")
+
+console.log ("O paulo fez esse commit se voce ver na aula!")
+console.log ("enviando para o github")
